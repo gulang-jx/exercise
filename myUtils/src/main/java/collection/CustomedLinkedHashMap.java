2998870da7a097fa0747db1bb033ed43ddd5e9e9
zhouliang <1719939724@qq.com>
2018-11-15 10:50:40 +0800

    定长LinkedHashMap

---
+++ myUtils/src/main/java/collection/CustomedLinkedHashMap.java
@@ -0,0 +1,19 @@
+package collection;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * 实现一个定长的有序的hashMap，通过在构造方法中传入size指定该对象存储数据的数量
+ */
+public class CustomedLinkedHashMap extends LinkedHashMap {
+    private int size;
+    public CustomedLinkedHashMap(){}
+    public CustomedLinkedHashMap(int size){
+        this.size = size;
+    }
+    @Override
+    protected boolean removeEldestEntry(Map.Entry eldest) {
+        return size>size();
+    }
+}
